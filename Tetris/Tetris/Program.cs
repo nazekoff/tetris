be2d6517df363f2f578ce6762f7ccef5a1e84922
postscript100@gmail.com <postscript100@gmail.com>
2020-05-08 17:30:51 +0600

    Создали проект Hello World

---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
