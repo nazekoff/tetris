b382955ee19ae2b4798fa143308d74a3f7272698
postscript100@gmail.com <postscript100@gmail.com>
2020-05-10 16:08:12 +0600

    Начали изучать ООП. Создали класс Figure

--- Tetris/Tetris/Square.cs
+++ Tetris/Tetris/Square.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace Tetris
 {
-    class Square
+    class Square : Figure
     {
-        Point[] points = new Point[4];
 
         public Square(int x, int y,  char sym)
         {
@@ -14,15 +13,6 @@ namespace Tetris
             points[1] = new Point(x + 1, y, sym);
             points[2] = new Point(x, y + 1, sym);
             points[3] = new Point(x + 1, y + 1, sym);
-        }
-
-        
-        public void Draw()
-        {
-            foreach (Point p in points)
-            {
-                p.Draw();
-            }
         }
 
     }
--- Tetris/Tetris/Stick.cs
+++ Tetris/Tetris/Stick.cs
@@ -4,10 +4,9 @@ using System.Text;
 
 namespace Tetris
 {
-    class Stick
+    class Stick : Figure
     {
-        Point[] points = new Point[4];
-
+        
         public Stick(int x, int y, char sym)
         {
             points[0] = new Point(x, y, sym);
@@ -15,14 +14,6 @@ namespace Tetris
             points[2] = new Point(x, y + 2, sym);
             points[3] = new Point(x, y + 3, sym);
         }
-
-
-        public void Draw()
-        {
-            foreach (Point p in points)
-            {
-                p.Draw();
-            }
-        }
+                
     }
 }
