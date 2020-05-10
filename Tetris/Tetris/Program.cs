0573ced50c5fec7ad46a199159315fed7f7567ab
postscript100@gmail.com <postscript100@gmail.com>
2020-05-10 16:09:43 +0600

    сохраню изменения

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -9,12 +9,14 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Square s = new Square(2, 3, '*');
-            s.Draw();
+            Figure[] f = new Figure[2];
+            f[0] = new Stick(8, 8, '*');
+            f[1] = new Square(2, 3, '*');
 
-
-            Stick stick = new Stick(8, 8, '*');
-            stick.Draw();
+            foreach (Figure fig in f)
+            {
+                fig.Draw();
+            }
 
 
             Console.ReadKey();
