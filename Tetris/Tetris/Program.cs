939adc64bc3bcc00ee8c2b10881f257036b0f22a
postscript100@gmail.com <postscript100@gmail.com>
2020-05-20 16:30:31 +0600

    Сделан генератор случайных фигур

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -10,37 +10,49 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Figure s = new Stick(20, 5, '*');
+            //Figure s = new Stick(20, 5, '*');
+            Figure[] f = new Figure[2];
+            f[0] = new Stick(20, 5, '*');
+            f[1] = new Square(25, 7, '*');
 
-            s.Draw();
+            Random rand = new Random();
+            int val = rand.Next(0, 2);
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            f[val].Draw();
 
+            
+            ////////////
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.LEFT);
-            s.Draw();
+            //s.Draw();
 
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Rotate();
+            //s.Draw();
 
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.RIGHT);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.LEFT);
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.DOWN);
+            //s.Draw();
+
+
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.RIGHT);
+            //s.Draw();
+
+
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Rotate();
+            //s.Draw();
 
 
             Console.ReadKey();
