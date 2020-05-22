8ac46e1a1bbbbec9641e7d09c65f104745ec5f99
postscript100@gmail.com <postscript100@gmail.com>
2020-05-22 16:02:58 +0600

    Создано движение фигур с помощью нажатия на клавиши.

--- Tetris/Tetris/Figure.cs
+++ Tetris/Tetris/Figure.cs
@@ -19,10 +19,12 @@ namespace Tetris
 
         public void Move(Direction dir)
         {
+            Hide();
             foreach (Point p in points)
             {
                 p.Move(dir);
             }
+            Draw();
         }
 
         public abstract void Rotate();
--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,32 +11,34 @@ namespace Tetris
             Console.SetBufferSize(40, 30);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure s = null;
+            Figure currentFigure = generator.GetNewFigure();
 
-
+            
             while (true)
             {
-                FigureFall(out s, generator);
-                s.Draw();
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    HandleKey(currentFigure, key);
+                }                                                
             }
 
         }
 
-        
-        static void FigureFall(out Figure fig, FigureGenerator generator)
+        private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
         {
-            fig = generator.GetNewFigure();
-            fig.Draw();
-
-            for (int i = 0; i < 20; i++)
+            switch (key.Key)
             {
-                Thread.Sleep(200);
-                fig.Hide();
-                fig.Move(Direction.DOWN);
-                fig.Draw();
+                case ConsoleKey.LeftArrow:
+                    currentFigure.Move(Direction.LEFT);
+                    break;
+                case ConsoleKey.RightArrow:
+                    currentFigure.Move(Direction.RIGHT);
+                    break;
+                case ConsoleKey.DownArrow:
+                    currentFigure.Move(Direction.DOWN);
+                    break;
             }
-            fig.Hide();
-        } 
-                
+        }
     }
 }
